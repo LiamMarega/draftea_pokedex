2deea6e4a67ed93b030a8a9a5759399700b403d9
LiamMarega <liammarega85@gmail.com>
2025-12-25 18:36:27 -0300

    feat: add initial Pokedex home and Pokemon details UI pages

---
+++ lib/pokedex/ui/pages/pokedex_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PokedexHome extends StatelessWidget {
+  const PokedexHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
