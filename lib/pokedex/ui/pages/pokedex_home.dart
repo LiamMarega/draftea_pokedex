8d1e4b224e81724eae430f2734b06506ab06310f
LiamMarega <liammarega85@gmail.com>
2025-12-26 14:43:38 -0300

    feat: add and us  responsive_grid_list  dependency to make responsive grid

--- lib/pokedex/ui/pages/pokedex_home.dart
+++ lib/pokedex/ui/pages/pokedex_home.dart
@@ -1,6 +1,7 @@
 import 'package:draftea_pokedex/core/router/route.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:responsive_grid_list/responsive_grid_list.dart';
 
 class PokedexHomePage extends StatelessWidget {
   const PokedexHomePage({super.key});
@@ -8,12 +9,30 @@ class PokedexHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        itemBuilder: (context, index) {
-          return const Text('Pokemon');
+      body: ResponsiveGridListBuilder(
+        horizontalGridSpacing: 16, // Horizontal space between grid items
+        verticalGridSpacing: 16, // Vertical space between grid items
+        horizontalGridMargin: 50, // Horizontal space around the grid
+        verticalGridMargin: 50, // Vertical space around the grid
+        minItemWidth:
+            300, // The minimum item width (can be smaller, if the layout constraints are smaller)
+        minItemsPerRow:
+            2, // The minimum items to show in a single row. Takes precedence over minItemWidth
+        maxItemsPerRow:
+            5, // The maximum items to show in a single row. Can be useful on large screens
+        gridItems: [
+          Container(child: Text('Pokemon')),
+          Text('Pokemon'),
+          Text('Pokemon'),
+          Text('Pokemon'),
+          Text('Pokemon'),
+          Text('Pokemon'),
+          Text('Pokemon'),
+        ], // The list of widgets in the grid
+        builder: (context, items) {
+          return Column(
+            children: items,
+          );
         },
       ),
     );
